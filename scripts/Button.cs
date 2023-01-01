09230893476d5bc74f92e3c5cdf226222fe781c5
Enes <79157030+ensaktrn@users.noreply.github.com>
2023-01-01 10:52:20 +0300

    Update Button.cs

--- scripts/Button.cs
+++ scripts/Button.cs
@@ -17,7 +17,7 @@ public class Button : MonoBehaviour
     public void OnButtonClick()
     {
         // 12 derece hata payı bıraktım.
-        if( (x<= 6 || (x>=84 && x<=106) || (x>=174 && x<=186) || (x>=264 && x<=276) || (x>=354 && x<=360)) && !isHit ){
+        if( (x<= 6 || (x>=84 && x<=96) || (x>=174 && x<=186) || (x>=264 && x<=276) || (x>=354 && x<=360)) && !isHit ){
             counter.IncreaseCounter();
             isHit= true;
             StartCoroutine(ResetHit()); 
